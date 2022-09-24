65f1540f96f339384c8ea379dee5d40039840c18
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-24 11:06:39 +0600

    added middleware

--- myApp.js
+++ myApp.js
@@ -1,39 +1,38 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+require("dotenv").config({ path: "./config.env" });
 
+console.log("Hello World");
+const path = __dirname + "/views/index.html";
 
+//Middleware
+const middleware = (req, res, next) => {
+  const method = req.method;
+  const path = req.path;
+  const ip = req.ip;
+  console.log(`${method} ${path} - ${ip}`);
+  next();
+};
+app.use(middleware);
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  // res.send("Hello Express");
+  // console.log("Hello express");
+  res.sendFile(path);
+});
 
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({
+      message: "HELLO JSON",
+    });
+  } else {
+    res.json({
+      message: "Hello json",
+    });
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
